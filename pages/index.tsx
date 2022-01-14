f9ab654f06b6e37aa85fdfac8141bcc9f6cb4fef
Cameron Tredoux <camtredoux@gmail.com>
2022-01-14 16:44:26 -0700

    Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,40 +6,45 @@ export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className="font-body flex flex-col items-center justify-center min-h-screen py-2 bg-gray-300">
+    <div className="font-body flex flex-col gap-5 items-center justify-center min-h-screen py-2 bg-gray-300">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {auth?.user ? (
-        <button
-          onClick={() => console.log(auth?.signout())}
-          className="font-bold hover:rounded-lg text-sm text-white hover:text-gray-900 transition ease duration-150 hover:bg-white py-1 px-4 rounded border-2 border-white bg-sky-300 shadow-sm hover:shadow-md"
-        >
-          Sign Out
-        </button>
-      ) : (
-        <button
-          onClick={() => console.log(auth?.signinWithGitHub())}
-          className="font-bold hover:rounded-lg text-sm text-white hover:text-gray-900 transition ease duration-150 hover:bg-white py-1 px-4 rounded border-2 border-white bg-sky-300 shadow-sm hover:shadow-md"
-        >
-          Sign In
-        </button>
-      )}
-      <div>Current user: {auth?.user?.email}</div>
       <div>
-        <Image
-          className="rounded-full"
-          width="80"
-          height="80"
-          src={
-            auth?.user?.photoURL
-              ? auth.user.photoURL
-              : "https://avatars.githubusercontent.com/u/424443?v=4"
-          }
-          alt={auth?.user?.displayName ? auth.user.displayName : "Profile"}
-        />
+        {auth?.user ? (
+          <button
+            onClick={() => console.log(auth?.signout())}
+            className="font-bold hover:rounded-lg text-sm text-white hover:text-gray-900 transition ease duration-150 hover:bg-white py-[0.375rem] px-4 rounded border-2 border-white bg-sky-300 shadow-sm hover:shadow-md"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={() => console.log(auth?.signinWithGitHub())}
+            className="font-bold hover:rounded-lg text-sm text-white hover:text-gray-900 transition ease duration-150 hover:bg-white py-[0.375rem] px-4 rounded border-2 border-white bg-sky-300 shadow-sm hover:shadow-md"
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+      <div className="flex items-center gap-2">
+        Current user:{" "}
+        <code className="font-body p-2 text-sm shadow-sm bg-white rounded">
+          {auth?.user?.email ? auth.user.email : "No user logged in"}
+        </code>
+        <div className="shadow-md w-10 h-10 rounded-full border-2 border-white">
+          {auth?.user?.photoURL ? (
+            <Image
+              className="rounded-full"
+              width="96"
+              height="96"
+              src={auth.user.photoURL}
+              alt={auth?.user?.displayName ? auth.user.displayName : "Profile"}
+            />
+          ) : null}
+        </div>
       </div>
     </div>
   );
