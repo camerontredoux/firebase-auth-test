29bbd4b296f2dbdf0e999087201a6b4dc81468ab
Cameron Tredoux <camtredoux@gmail.com>
2022-01-14 02:39:23 -0700

    Lingering import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { AuthProvider } from "../utils/auth";
+// import { AuthProvider } from "../utils/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
