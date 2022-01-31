c54e2ecb519d7fa8bf0a2d24a4cd2b6f81724a20
Cameron Tredoux <camtredoux@gmail.com>
2022-01-30 17:50:26 -0700

    Added pagination for rickandmortyapi (swr and staticprops)

--- src/pages/ram/index.tsx
+++ src/pages/ram/index.tsx
@@ -1,30 +1,66 @@
 import RAMCard from "@/components/RAMCard";
 import { CharacterType } from "@/utils/CharacterType";
-import { GetServerSideProps } from "next";
-import React from "react";
+import { GetStaticProps } from "next";
+import React, { useState } from "react";
+import useSWR, { SWRConfig } from "swr";
 
-interface RickandmortyProps {
-  results: CharacterType;
-}
+const fetcher = async (input: RequestInfo, init: RequestInit) =>
+  await fetch(input, init).then((res) => res.json());
 
-const Rickandmorty: React.FC<RickandmortyProps> = ({ results }) => {
-  const characters = results.results;
+const Rickandmorty = () => {
+  const [index, setIndex] = useState(1);
+  const { data } = useSWR<CharacterType>(
+    `https://rickandmortyapi.com/api/character?page=${index}`,
+    fetcher
+  );
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {characters && characters.map((c) => <RAMCard key={c.id} c={c} />)}
-    </ul>
+    <>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {data?.results.map((c) => (
+          <RAMCard key={c.id} c={c} />
+        ))}
+      </ul>
+      <div className="flex items-center justify-center mt-4 gap-2">
+        <button
+          className="rounded-md p-2 bg-slate-400"
+          onClick={() => setIndex((i) => i + 1)}
+        >
+          Next Page
+        </button>
+        <button
+          className="rounded-md p-2 bg-slate-400"
+          onClick={() => setIndex((i) => (i > 1 ? i - 1 : i))}
+        >
+          Previous Page
+        </button>
+      </div>
+    </>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const data = await fetch("https://rickandmortyapi.com/api/character");
   const results = await data.json();
   return {
     props: {
-      results,
+      fallback: {
+        results,
+      },
     },
   };
 };
 
-export default Rickandmorty;
+interface PageProps {
+  fallback: CharacterType[];
+}
+
+const Page: React.FC<PageProps> = ({ fallback }) => {
+  return (
+    <SWRConfig value={{ fallback }}>
+      <Rickandmorty />
+    </SWRConfig>
+  );
+};
+
+export default Page;
